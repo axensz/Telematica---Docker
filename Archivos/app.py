ac0158df4cacb881c30e820c8d55c037a478dbd7
Camilo Guzmán <89235206+axensz@users.noreply.github.com>
2022-11-19 12:07:44 -0600

    Version 01.1

--- Archivos/app.py
+++ Archivos/app.py
@@ -3,16 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('./index.php')
+def index():
+    return render_template('index.php')
 
 @app.route('/registro')
 def registro():
-    return render_template('./registro.php')
+    return render_template('registro.php')
 
 @app.route('/iniciosesion')
 def login():
-    return render_template('./login.php')
+    return render_template('login.php')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=80)
+    app.run(host='0.0.0.0', port=80, debug=True)
